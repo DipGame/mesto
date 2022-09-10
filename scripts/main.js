a7bd51d610f93b05b3708b510f18473899a7bfb2
IVACHEV KONSTANTIN <kostya1.6@yandex.ru>
2022-09-10 12:38:39 +0500

    Praktika

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const overlayEl = document.querySelector('.overlay');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const closePopupButton = document.querySelector('.popup__close-button');
+const savePopupButton = overlayEl.querySelector('.popup__save-button');
+
+const toggleOverlay = () => {
+    overlayEl.classList.toggle('overlay__open');
+}
+
+openPopupButton.addEventListener('click', () => {
+    toggleOverlay();
+})
+
+closePopupButton.addEventListener('click', () => {
+    toggleOverlay();
+})
+
+savePopupButton.addEventListener('click', () => {
+    toggleOverlay();
+})
