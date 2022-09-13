6146c6e2d5b5703038262cb08111b0927b73899c
IVACHEV KONSTANTIN <kostya1.6@yandex.ru>
2022-09-13 19:47:24 +0500

    1.01

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 const overlayEl = document.querySelector('.overlay');
+const popupName = overlayEl.querySelector('.popup__name');
+const popupProf = overlayEl.querySelector('.popup__profession');
 const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__close-button');
 const savePopupButton = overlayEl.querySelector('.popup__save-button');
+
+let profile = document.querySelector('.profile');
+let profileName = profile.querySelector(".profile__name");
+let profileProf = profile.querySelector(".profile__profession");
 
 const toggleOverlay = () => {
     overlayEl.classList.toggle('overlay__open');
@@ -14,3 +20,11 @@ openPopupButton.addEventListener('click', () => {
 closePopupButton.addEventListener('click', () => {
     toggleOverlay();
 })
+
+savePopupButton.addEventListener('click', () => {
+    profileName.textContent = popupName.value;
+    profileProf.textContent = popupProf.value;
+    toggleOverlay();
+})
+
+overlayEl.addEventListener('submit', savePopupButton);
