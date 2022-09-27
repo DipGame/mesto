653e6b2eb0e954b676a26bcf8b148e4a95a04298
IVACHEV KONSTANTIN <kostya1.6@yandex.ru>
2022-09-28 00:30:47 +0500

    JAVAAAAAA

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,6 @@ let popupName = overlayEl.querySelector('input[name="name"]');
 let popupProf = overlayEl.querySelector('input[name="profession"]');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close-button');
-let savePopupButton = overlayEl.querySelector('.popup__save-button');
 let profile = document.querySelector('.profile');
 let profileName = profile.querySelector(".profile__name");
 let profileProf = profile.querySelector(".profile__profession");
@@ -54,7 +53,6 @@ let placeName = overlayEl.querySelector('input[name="placeName"]');
 let placeUrl = overlayEl.querySelector('input[name="placeUrl"]');
 let openPlaceButton = document.querySelector('.profile__add-button');
 let closePlaceButton = document.querySelector('.place__close-button');
-let addPlaceButton = overlayEl.querySelector('.place__add-button');
 
 let togglePlaceOverlay = () => {
     placeOverlayEl.classList.toggle('place-overlay_open');
@@ -64,7 +62,68 @@ openPlaceButton.addEventListener('click', togglePlaceOverlay);
 
 closePlaceButton.addEventListener('click', togglePlaceOverlay);
 
+let initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+function placeAdd (evt) {
+    evt.preventDefault();
+
+    initialCards.unshift({
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    });
+
+    togglePlaceOverlay();
+}
+
+placeOverlayEl.addEventListener('submit', placeAdd);
+
+let cardItem = ''
+
+let out = document.getElementById('elements')
+initialCards.forEach ((menu) => {
+    cardItem +=
+    `
+<div class="element">
+    <button class="element__delete" type="button"></button>
+    <img class="element__image" src="${menu.link}" alt="Картинка">
+    <div class="element__container-bottom">
+        <h2 class="element__title">${menu.name}</h2>
+        <button class="element__like" type="button"></button>
+    </div>
+</div>
+    `
+})
+
+out.insertAdjacentHTML('afterbegin', cardItem);
+
 
 
 //Конец Place(Типа попапа, только для добавления новых картинок)
+
+
 
