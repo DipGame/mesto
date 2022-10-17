79307a05b96832e3dc87a767b55bd68d9ec2b578
Ivachev Konstantin <kostya1.6@yandex.ru>
2022-10-17 15:37:37 +0500

    ne rabotaet dobavlenir kartochki

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ let overlayEl = document.querySelector('.overlay');
 let popupForm = overlayEl.querySelector('.popup')
 let popupName = overlayEl.querySelector('input[name="name"]');
 let popupProf = overlayEl.querySelector('input[name="profession"]');
-let openPopupButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button');
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
 let profile = document.querySelector('.profile');
 let profileName = profile.querySelector(".profile__name");
 let profileProf = profile.querySelector(".profile__profession");
@@ -17,11 +17,11 @@ let toggleOverlay = () => {
     overlayEl.classList.toggle('overlay_open');
 }
 
-openPopupButton.addEventListener('click', toggleOverlay);
+popupOpenButton.addEventListener('click', toggleOverlay);
 
-closePopupButton.addEventListener('click', toggleOverlay);
+popupCloseButton.addEventListener('click', toggleOverlay);
 
-function formSubmitHandler (evt) {
+function submitFormHandler (evt) {
     evt.preventDefault();
 
     profileName.textContent = popupName.value;
@@ -30,7 +30,7 @@ function formSubmitHandler (evt) {
     toggleOverlay();
 }
 
-overlayEl.addEventListener('submit', formSubmitHandler);
+overlayEl.addEventListener('submit', submitFormHandler);
 //Конец Попапа для редактирования профиля
 
 
@@ -66,14 +66,15 @@ const initialCards = [
   }
 ];
 
-
+  const elementsTemplate = document.querySelector('.elements');
+  const placeTemplate = document.querySelector('.placeTemplate').content;
   const placeOverlayEl = document.querySelector('.place-overlay');
   const placeForm = placeOverlayEl.querySelector('.place');
   const placeName = placeOverlayEl.querySelector('input[name="placeName"]');
   const placeUrl = placeOverlayEl.querySelector('input[name="placeUrl"]');
-  const openPlaceButton = document.querySelector('.profile__add-button');
-  const savePlaceButton = placeOverlayEl.querySelector('.place__add-button');
-  const closePlaceButton = placeOverlayEl.querySelector('.place__close-button');
+  const placeOpenButton = document.querySelector('.profile__add-button');
+  const placeSaveButton = placeOverlayEl.querySelector('.place__add-button');
+  const placeCloseButton = placeOverlayEl.querySelector('.place__close-button');
 
   const imgOverlayEl = document.querySelector('.img-overlay');
   const imgForm = imgOverlayEl.querySelector('.img-form');
@@ -81,86 +82,75 @@ const initialCards = [
   const imgCloseButton = imgOverlayEl.querySelector('.img-form__close-button');
   const imgPicture = imgOverlayEl.querySelector('.img-form__picture');
 
-const placeToggleOverlay = () => {
-  placeOverlayEl.classList.toggle('place-overlay_open');
+function render() { //функция рендер карточек из массива
+  initialCards.forEach(createPlace);
 }
 
-const imgToggleOverlay = () => {
-  imgOverlayEl.classList.toggle('img-overlay_open');
-}
-
-openPlaceButton.addEventListener('click', placeToggleOverlay);
-closePlaceButton.addEventListener('click', placeToggleOverlay);
-
-
-placeOverlayEl.addEventListener('submit', addCard);
-
-const elementsTemplate = document.querySelector('.elements');
-const placeTemplate = document.querySelector('.placeTemplate').content;
-
-
-
-
-
-function render() {
-  initialCards.forEach(loadPlace);
-}
-
-function loadPlace({name, link}) {
+function createPlace({name, link}) { //функция отрисовки карточек
   const copyPlace = placeTemplate.cloneNode(true);
   const placeTitle = copyPlace.querySelector('.element__title');
   const img = copyPlace.querySelector('.element__image');
   const likeButton = copyPlace.querySelector('.element__like');
+  const btnDelete = copyPlace.querySelector('.element__delete');
+
   img.src = link; 
   placeTitle.textContent = name; 
 
-document.querySelector('.elements').onclick = function(e) {
-  const btn = e.target.closest('.element__delete');
-      if (!btn) {
-      return;
-  }
-      btn.closest('.element').remove();
-  }
+  btnDelete.addEventListener('click', handleDeleteCard); //кнопка удаления
 
-img.addEventListener("click", openPicture);
-imgCloseButton.addEventListener("click", imgToggleOverlay);
+  img.addEventListener("click", openPicture);//кнопка открытия картинки по нажатию на картинку
+  imgCloseButton.addEventListener('click', closeImgOverlay);//кнопка закрытия картинки
 
-likeButton.addEventListener("click", likeActive);
-
-  // не забудь листенеры еще для карточки (лайк, иконка корзинки, картинка), если в текущей ПР требуется это делать
-  // вот тут...
-  
-  // только потом вставляй карточку в разметку
-  elementsTemplate.appendChild(copyPlace);
+  likeButton.addEventListener("click", handleLikeActive);//кнопка лайк
+  appendChild(copyPlace);
 }
 
-function openPicture(event) {
+function appendChild (name) {
+  elementsTemplate.appendChild(name);
+}
+
+function newCard(name) {//функция добавления новых карточек
+  createPlace();
+  link = placeUrl.value;  
+  name = placeName.value;  
+  elementsTemplate.prepend(name);
+}
+
+placeSaveButton.addEventListener('click', newCard);
+
+placeOpenButton.addEventListener('click', openPlaceOverlay);
+placeCloseButton.addEventListener('click', closePlaceOverlay);
+
+function openPlaceOverlay() {//функция открытия попапа для добавления картинок
+  placeOverlayEl.classList.toggle('place-overlay_open');
+}
+
+function closePlaceOverlay() {//функция закрытия попапа для добавления картинок
+  placeOverlayEl.classList.remove('place-overlay_open');
+}
+
+function openImgOverlay(popup) {//функция открытия попапа для картинок
+  imgOverlayEl.classList.toggle(popup);
+}
+
+function closeImgOverlay() {//функция закрытия попапа для картинок
+  imgOverlayEl.classList.remove('img-overlay_open');
+}
+
+const handleDeleteCard = (evt) => { //функция работы кнопки удаления 
+  evt.target.closest('.element').remove();
+}
+
+function openPicture(event) {//функция открытия картинки по нажатию на картинку
   const imgElementTg = event.target.closest('.element');
   imgPicture.src = event.target.src;
   imgName.textContent = imgElementTg.textContent;
-  imgToggleOverlay();
+  openImgOverlay('img-overlay_open');
 }
 
-function likeActive(event) {
+function handleLikeActive(event) {//функция лайка
   event.target.classList.toggle('element__like_active');
 }
-
-
-
-function addCard(evt) {
-  evt.preventDefault();
-  const copyPlace = placeTemplate.cloneNode(true);
-  const placeTitle = copyPlace.querySelector('.element__title');
-  const img = copyPlace.querySelector('.element__image');
-  img.src = placeUrl.value; 
-  placeTitle.textContent = placeName.value; 
-
-  elementsTemplate.prepend(copyPlace);
-
-  placeToggleOverlay();
-}
-
-
 
 render();
 //Конец Place(Типа попапа, только для добавления новых картинок)
